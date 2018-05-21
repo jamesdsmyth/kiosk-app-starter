6e7ebbd352bf28559bf924ba23391966a7998b42
James <smythie_89@hotmail.com>
2018-05-21 13:01:51 +0530

    Added more styles to the landing images

--- app/components/molecules/LandingPageGridItem/LandingPageGridItem.js
+++ app/components/molecules/LandingPageGridItem/LandingPageGridItem.js
@@ -1,24 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 /*
   The component renders a landing page grid item.
  */
-const LandingPageGridItem = ({ isPrimary, size }) => {
+const LandingPageGridItem = ({
+  title,
+  subtitle,
+  image,
+  urlLink,
+  isPrimary,
+  size
+}) => {
   const classes = isPrimary ? `primary-grid-item__${size}` : null;
+  const subtitleElement = isPrimary ? <span>{subtitle}</span> : null;
+  const itemStyle = {
+    backgroundImage: `url(${image})`
+  };
+
   return (
-    <div className={`grid-item ${classes}`}>
-      grid item
+    <div
+      className={`grid-item ${classes}`}
+      style={itemStyle}
+    >
+      <Link
+        to={urlLink}
+        className="link-wrapper"
+      >
+        <h2>{title}</h2>
+        {subtitleElement}
+      </Link>
     </div>
   );
 };
 
 LandingPageGridItem.defaultProps = {
+  subtitle: '',
   isPrimary: false,
   size: null
 };
 
 LandingPageGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  urlLink: PropTypes.string.isRequired,
   isPrimary: PropTypes.bool,
   size: PropTypes.number
 };
--- app/components/organisms/LandingPageGrid/LandingPageGrid.js
+++ app/components/organisms/LandingPageGrid/LandingPageGrid.js
@@ -1,44 +1,150 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LandingPageGridItem from '../../molecules/LandingPageGridItem/LandingPageGridItem';
 
 /*
   The component renders a landing page grid.
  */
-const LandingPageGrid = () => (
-  <div>
-    <section className="grid grid__3">
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-    </section>
-    <section className="grid grid__4">
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-    </section>
-    <section className="grid grid__7">
-      <LandingPageGridItem isPrimary size={7} />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-    </section>
-    <section className="grid grid__10">
-      <LandingPageGridItem isPrimary size={10} />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-      <LandingPageGridItem />
-    </section>
-  </div>
-);
-
-export default LandingPageGrid;
+export default class LandingPageGrid extends Component {
+  render() {
+    return (
+      <div>
+        <section className="grid grid__3">
+          <LandingPageGridItem
+            title="Check the summer collection"
+            subtitle="25% off on dresses"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+            isPrimary
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+        </section>
+        <section className="grid grid__4">
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+        </section>
+        <section className="grid grid__7">
+          <LandingPageGridItem
+            title="Check the summer collection"
+            subtitle="25% off on dresses"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+            isPrimary
+            size={7}
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+        </section>
+        <section className="grid grid__10">
+          <LandingPageGridItem
+            title="Check the summer collection"
+            subtitle="25% off on dresses"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+            isPrimary
+            size={10}
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+          <LandingPageGridItem
+            title="Check the summer collection"
+            image="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+            urlLink="/"
+          />
+        </section>
+      </div>
+    );
+  }
+}
